48b9f6dba9342ce4c0f7202d74bef2373f80acaf
Prabal Dutta <wizard19@gmail.com>
2015-05-26 03:02:04 -0500

    added resume and fixed the footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get "blog" => "pages#blog" 
+  get "resume" => "pages#resume" 
   get "contact" => "pages#contact"
 
   # The priority is based upon order of creation: first created -> highest priority.
